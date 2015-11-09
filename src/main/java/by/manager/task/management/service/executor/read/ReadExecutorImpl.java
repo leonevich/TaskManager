c892c7b0753d94de7b158fb87fd39e1797c762d9
Leanid Ivashkevich <leonevich@gmail.com>
2015-11-09 15:11:53 +0300

    add the property of file path

--- src/main/java/by/manager/task/management/service/executor/read/ReadExecutorImpl.java
+++ src/main/java/by/manager/task/management/service/executor/read/ReadExecutorImpl.java
@@ -5,15 +5,19 @@ import by.manager.task.utils.beans.TaskVO;
 import by.manager.task.utils.enums.TaskStatus;
 import by.manager.task.utils.enums.TaskType;
 import by.manager.task.utils.errors.exceptions.executor.read.ReadExecutorException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.Properties;
 
 @Component
 public class ReadExecutorImpl implements TaskExecutor {
 
     private final static TaskType TASK_TYPE = TaskType.READ;
 
+    @Autowired
+    private Properties properties;
     @Override
     public TaskType getTaskType() {
         return TASK_TYPE;
@@ -22,7 +26,7 @@ public class ReadExecutorImpl implements TaskExecutor {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
     public TaskVO executeTask(TaskVO taskVO) throws ReadExecutorException {
-        File file = new File("d:\\files_to_work\\");
+        File file = new File(properties.getProperty("file.path"));
         File[] files = file.listFiles();
         if ((files != null ? files.length : 0) != 0) {
             for (File currentFile : files) {
--- src/main/java/by/manager/task/management/service/executor/write/WriteExecutorImpl.java
+++ src/main/java/by/manager/task/management/service/executor/write/WriteExecutorImpl.java
@@ -5,10 +5,12 @@ import by.manager.task.management.service.executor.TaskExecutor;
 import by.manager.task.utils.beans.TaskVO;
 import by.manager.task.utils.enums.TaskStatus;
 import by.manager.task.utils.enums.TaskType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 @Component
 public class WriteExecutorImpl implements TaskExecutor {
@@ -20,12 +22,15 @@ public class WriteExecutorImpl implements TaskExecutor {
         return taskType;
     }
 
+    @Autowired
+    private Properties properties;
+
     @Override
     public TaskVO executeTask(TaskVO taskVO) {
-        String fullFileName = "d:\\files_to_work\\" + taskVO.getName() + ".txt";
-        String text = "Any file";
+        String fullFileName = properties.getProperty("file.path") + taskVO.getName() + "." + properties.getProperty("file.expansion");
+        String content = properties.getProperty("file.content");
         try (FileOutputStream fileOutputStream = new FileOutputStream(fullFileName)) {
-            byte[] buffer = text.getBytes();
+            byte[] buffer = content.getBytes();
             fileOutputStream.write(buffer, 0, buffer.length);
             taskVO.setStatus(TaskStatus.COMPLETED);
             return taskVO;
